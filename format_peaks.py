bff2de59b139c09da343988213c83e4d5325a080
m1kss <boytsovs.av@phystech.edu>
2020-09-29 23:54:21 +0300

    filter best 1000 peaks

--- format_peaks.py
+++ format_peaks.py
@@ -22,30 +22,58 @@ peaks_types = {
     'sissrs'
 }
 
+score_types = {
+    'score',
+    'pvalue'
+}
+
 
 def check_row(df_row):
     return (df_row['#CHR'] in ChromPos.chrs) and df_row['START'] > 0 and df_row['END'] > 0
 
 
-def main(peak_file_name, peak_type, out_path):
+def main(peak_file_name, peak_type, score_type, out_path):
     assert peak_type in peaks_types
+    assert score_type in score_types
     peak_df = pd.read_table(peak_file_name)
     if peak_df.empty:
         print('empty peaks, {}'.format(peak_file_name))
         exit(1)
     if peak_type == 'cpics' or peak_type == 'sissrs':
         peak_df['SUMMIT'] = (peak_df['END'] - peak_df['START']) // 2
+        if peak_type == 'cpics':
+            assert score_type == 'score'
+            peak_df['SCORE'] = peak_df['score']
+        elif peak_type == 'sissrs':
+            if score_type == 'score':
+                peak_df['SCORE'] = peak_df['NumTags']
+            elif score_type == 'pvalue':
+                peak_df['SCORE'] = peak_df['p-value']
     elif peak_type == 'macs':
         peak_df['SUMMIT'] = peak_df['summit']
+        if score_type == 'score':
+            peak_df['SCORE'] = peak_df['tags']
+        elif score_type == 'pvalue':
+            peak_df['SCORE'] = peak_df['-10*log10(pvalue)']
     elif peak_type == 'gem':
         peak_df['SUMMIT'] = peak_df['END']
         peak_df['START'] = peak_df['START'].apply(lambda x: max(x - 150, 0))
         peak_df['END'] = peak_df['END'] + 150
+        if score_type == 'score':
+            peak_df['SCORE'] = peak_df['IP']
+        elif score_type == 'pvalue':
+            peak_df['SCORE'] = peak_df['P_-lg10']
     elif peak_type == 'macs2' or peak_type == 'macs2-nomodel':
         peak_df['SUMMIT'] = peak_df['abs_summit'] - peak_df['START']
+        if score_type == 'score':
+            peak_df['SCORE'] = peak_df['pileup']
+        elif score_type == 'pvalue':
+            peak_df['SCORE'] = peak_df['-log10(pvalue)']
 
     peak_df['#CHR'] = peak_df['#CHROM'].apply(lambda x: 'chr' + str(x))
     peak_df = peak_df[peak_df.apply(check_row, axis=1)]
+    score_tr = peak_df.sort_values('SCORE', ascending=False)['SCORE'][999]
+    peak_df = peak_df[peak_df['SCORE'] >= score_tr]
     if peak_df.empty:
         print('empty peaks, {}'.format(peak_file_name))
         exit(1)
@@ -53,4 +81,4 @@ def main(peak_file_name, peak_type, out_path):
 
 
 if __name__ == '__main__':
-    main(peak_file_name=os.path.expanduser(sys.argv[1]), peak_type=sys.argv[2], out_path=os.path.expanduser(sys.argv[3]))
+    main(peak_file_name=os.path.expanduser(sys.argv[1]), peak_type=sys.argv[2], score_type=sys.argv[3], out_path=os.path.expanduser(sys.argv[4]))
--- process_peaks.sh
+++ process_peaks.sh
@@ -13,16 +13,23 @@ fi
 for peak_dir in $peaks_path/*
 do
   peak_type=$(basename $peak_dir)
-  if ! python3 format_peaks.py $peaks_path/$peak_type/$peaks_name.interval $peak_type $out_path/raw/${peaks_name}.${peak_type}.bed 1>$out_path/logs/${peaks_name}.${peak_type}.format.log 2>&1
-  then
-    continue
-  fi
-  bedtools sort -i $out_path/raw/${peaks_name}.${peak_type}.bed 1> $out_path/sorted/${peaks_name}.${peak_type}.sorted.bed 2>$out_path/logs/${peaks_name}.${peak_type}.sort.log
-  if ! python3 cut_fasta.py $genome $out_path/sorted/${peaks_name}.${peak_type}.sorted.bed $out_path/fasta/${peaks_name}.${peak_type}.mfa 1>$out_path/logs/${peaks_name}.${peak_type}.cut.log 2>&1
-  then
-    continue
-  fi
+  for score_type in "score pvalue"
+  do
+    if [ $peak_type == "cpics" ] && [ $score_type == 'pvalue' ]
+    then
+      continue
+    fi
+    if ! python3 format_peaks.py $peaks_path/$peak_type/$peaks_name.interval $peak_type $score_type $out_path/raw/${peaks_name}.${peak_type}.${score_type}.bed 1>$out_path/logs/${peaks_name}.${peak_type}.${score_type}.format.log 2>&1
+    then
+      continue
+    fi
+    bedtools sort -i $out_path/raw/${peaks_name}.${peak_type}.${score_type}.bed 1> $out_path/sorted/${peaks_name}.${peak_type}.${score_type}.sorted.bed 2>$out_path/logs/${peaks_name}.${peak_type}.${score_type}.sort.log
+    if ! python3 cut_fasta.py $genome $out_path/sorted/${peaks_name}.${peak_type}.${score_type}.sorted.bed $out_path/fasta/${peaks_name}.${peak_type}.${score_type}.mfa 1>$out_path/logs/${peaks_name}.${peak_type}.${score_type}.cut.log 2>&1
+    then
+      continue
+    fi
 
-  java -Xmx4G -cp chipmunk.jar ru.autosome.di.ChIPHorde 7:22,7:22 f c 1.0 m:$out_path/fasta/${peaks_name}.${peak_type}.mfa 400 40 1 2 random auto single 1>$out_path/results/${peaks_name}.${peak_type}.single.out 2>$out_path/logs/${peaks_name}.${peak_type}.single.log
-  java -Xmx4G -cp chipmunk.jar ru.autosome.di.ChIPHorde 22:7,22:7 f c 1.0 m:$out_path/fasta/${peaks_name}.${peak_type}.mfa 400 40 1 2 random auto 1>$out_path/results/${peaks_name}.${peak_type}.flat.out 2>$out_path/logs/${peaks_name}.${peak_type}.flat.log
+    java -Xmx4G -cp chipmunk.jar ru.autosome.di.ChIPHorde 7:22,7:22 f c 1.0 m:$out_path/fasta/${peaks_name}.${peak_type}.${score_type}.mfa 400 40 1 2 random auto single 1>$out_path/results/${peaks_name}.${peak_type}.${score_type}.single.out 2>$out_path/logs/${peaks_name}.${peak_type}.${score_type}.single.log
+    java -Xmx4G -cp chipmunk.jar ru.autosome.di.ChIPHorde 22:7,22:7 f c 1.0 m:$out_path/fasta/${peaks_name}.${peak_type}.${score_type}.mfa 400 40 1 2 random auto 1>$out_path/results/${peaks_name}.${peak_type}.${score_type}.flat.out 2>$out_path/logs/${peaks_name}.${peak_type}.${score_type}.flat.log
+  done
 done
