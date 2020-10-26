aa5895e031ec27629301226f37732052f3bf30d0
m1kss <boytsovs.av@phystech.edu>
2020-10-26 23:07:22 +0300

    results parsing

--- parse_results.py
+++ parse_results.py
@@ -1,6 +1,14 @@
 import os
 import json
 
+def get_time(time):
+    if time < 60:
+        return str(time) + 's'
+    elif time < 3600:
+        return str(round(time / 60)) + 'm'
+    else:
+        return str(round(time / 3600)) + 'h'
+
 results_dir = os.path.expanduser('~/results_SP1/')
 outputs_dir = os.path.join(results_dir, 'results')
 pcms_dir = os.path.expanduser('~/pcms_SP1/')
@@ -23,12 +31,22 @@ for file in os.listdir(outputs_dir):
     G = [value.split(' ') for key, value in parsed_output if key == 'G']
     T = [value.split(' ') for key, value in parsed_output if key == 'T']
     DIAG = [value for key, value in parsed_output if key == 'DIAG' and value.startswith('fail')]
-    if DIAG:
-        print(DIAG)
     if len(A) == 0:
         assert len(parsed_output) == 0
+        info.append({
+            'name': peaks,
+            'caller': caller,
+            'motif_type': motif_type,
+            'selected_by': best_by,
+            'motif_index': None,
+            'motif_len': None,
+            'diag': DIAG,
+            'time': None,
+        })
         continue
     ACGT = list(zip(A, C, G, T))
+    TIME = [int(value) for key, value in parsed_output if key == 'TIME'][-1]
+    time = get_time(time)
     for i, quad in enumerate(ACGT):
         if quad in ACGT[:i]:
             assert len(ACGT) == 2*i
@@ -37,8 +55,19 @@ for file in os.listdir(outputs_dir):
     for k, quad in enumerate(ACGT):
         with open(os.path.join(pcms_dir, '{}.{}.pcm'.format(file[:-4], k)), 'w') as pcm:
             pcm.write('>{}.{}'.format(file[:-4], k) + '\n')
-            for a, c, g, t in list(zip(*quad)):
-                pcm.write(''.join([a, c, g, t, '\n']))
-    # info.append({
-    #
-    # })
+            values = list(zip(*quad))
+            motif_len = len(values)
+            for a, c, g, t in values:
+                pcm.write('\t'.join([a, c, g, t]) + '\n')
+        info.append({
+            'name': peaks,
+            'caller': caller,
+            'motif_type': motif_type,
+            'selected_by': best_by,
+            'motif_index': k,
+            'motif_len': motif_len,
+            'diag': DIAG,
+            'time': time,
+        })
+with open(os.path.expanduser('~/info.json'), 'w') as ot:
+    json.dump(info, ot)
