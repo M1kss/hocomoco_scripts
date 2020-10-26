d96dc24b51768959ea079a84e6c64615803c5838
m1kss <boytsovs.av@phystech.edu>
2020-10-26 22:52:01 +0300

    results parsing

--- parse_results.py
+++ parse_results.py
@@ -3,6 +3,10 @@ import json
 
 results_dir = os.path.expanduser('~/results_SP1/')
 outputs_dir = os.path.join(results_dir, 'results')
+pcms_dir = os.path.expanduser('~/pcms_SP1/')
+
+if not os.path.isdir(pcms_dir):
+    os.mkdir(pcms_dir)
 
 info = []
 
@@ -14,10 +18,13 @@ for file in os.listdir(outputs_dir):
         for line in f:
             key, value = line.strip('\n').split('|')
             parsed_output.append((key, value))
-    A = [value for key, value in parsed_output if key == 'A']
-    C = [value for key, value in parsed_output if key == 'C']
-    G = [value for key, value in parsed_output if key == 'G']
-    T = [value for key, value in parsed_output if key == 'T']
+    A = [value.split(' ') for key, value in parsed_output if key == 'A']
+    C = [value.split(' ') for key, value in parsed_output if key == 'C']
+    G = [value.split(' ') for key, value in parsed_output if key == 'G']
+    T = [value.split(' ') for key, value in parsed_output if key == 'T']
+    DIAG = [value for key, value in parsed_output if key == 'DIAG' and value.startswith('fail')]
+    if DIAG:
+        print(DIAG)
     if len(A) == 0:
         assert len(parsed_output) == 0
         continue
@@ -25,4 +32,13 @@ for file in os.listdir(outputs_dir):
     for i, quad in enumerate(ACGT):
         if quad in ACGT[:i]:
             assert len(ACGT) == 2*i
+            ACGT = ACGT[:i]
             break
+    for k, quad in enumerate(ACGT):
+        with open(os.path.join(pcms_dir, '{}.{}.pcm'.format(file[:-4], k))) as pcm:
+            pcm.write('>{}.{}'.format(file[:-4], k) + '\n')
+            for a, c, g, t in zip(quad):
+                pcm.write(''.join([a, c, g, t, '\n']))
+    # info.append({
+    #
+    # })
