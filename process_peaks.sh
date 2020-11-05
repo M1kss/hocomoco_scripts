b8ff485cb80ef363d13d68593c09ab6854289b9c
m1kss <boytsovs.av@phystech.edu>
2020-11-05 21:20:09 +0300

    logs

--- process_peaks.sh
+++ process_peaks.sh
@@ -22,11 +22,17 @@ do
     fi
     if ! python3 format_peaks.py $peaks_path/$peak_type/$peaks_name.interval $peak_type $score_type $out_path/raw/${peaks_name}.${peak_type}.${score_type}.bed 1>$out_path/logs/${peaks_name}.${peak_type}.${score_type}.format.log 2>&1
     then
+      echo "Format peaks failed: $peaks_path/$peak_type/$peaks_name.interval"
       continue
     fi
-    bedtools sort -i $out_path/raw/${peaks_name}.${peak_type}.${score_type}.bed 1> $out_path/sorted/${peaks_name}.${peak_type}.${score_type}.sorted.bed 2>$out_path/logs/${peaks_name}.${peak_type}.${score_type}.sort.log
+    if ! bedtools sort -i $out_path/raw/${peaks_name}.${peak_type}.${score_type}.bed 1> $out_path/sorted/${peaks_name}.${peak_type}.${score_type}.sorted.bed 2>$out_path/logs/${peaks_name}.${peak_type}.${score_type}.sort.log
+    then
+      echo "Bed tools failed: $out_path/raw/${peaks_name}.${peak_type}.${score_type}.bed"
+      continue
+    fi
     if ! python3 cut_fasta.py $genome $out_path/sorted/${peaks_name}.${peak_type}.${score_type}.sorted.bed $out_path/fasta/${peaks_name}.${peak_type}.${score_type}.mfa 1>$out_path/logs/${peaks_name}.${peak_type}.${score_type}.cut.log 2>&1
     then
+      echo "Cut fasta failed: $out_path/sorted/${peaks_name}.${peak_type}.${score_type}.sorted.bed"
       continue
     fi
   done
