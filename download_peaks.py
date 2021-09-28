75983b1c92389d009a9abd94866ef7feb859bc59
wish1 <aswq22013@gmail.com>
2021-09-29 02:27:14 +0300

    some fixes

--- download_peaks.py
+++ download_peaks.py
@@ -7,13 +7,14 @@ from tqdm import tqdm
 out_path = os.path.expanduser('~/mouse/peaks')
 df = pd.read_table('~/mouse/mouse_peaks_dn.tsv')
 df = df[~df['DOWNLOAD_PATH'].isna() & (df['DOWNLOAD_PATH'] != 'None')]
-for index, row in tqdm(df.iterrows(), total=len(df.index)):
-    for peak_path in row['DOWNLOAD_PATH'].split():
-        peak_type = os.path.basename(os.path.dirname(peak_path))
-        dir_path = os.path.join(out_path, peak_type)
-        if not os.path.exists(dir_path):
-            os.mkdir(dir_path)
-        process = subprocess.Popen(['scp', '-P', '1300',
-                                    'autosome@localhost:' + peak_path,
-                                    dir_path, '>', 'scp0929.log'])
-        process.wait()
+with open('scp.log', 'w') as f:
+    for index, row in tqdm(df.iterrows(), total=len(df.index)):
+        for peak_path in row['DOWNLOAD_PATH'].split():
+            peak_type = os.path.basename(os.path.dirname(peak_path))
+            dir_path = os.path.join(out_path, peak_type)
+            if not os.path.exists(dir_path):
+                os.mkdir(dir_path)
+            process = subprocess.Popen(['scp', '-P', '1300',
+                                        'autosome@localhost:' + peak_path,
+                                        dir_path], stdout=f)
+            process.wait()
