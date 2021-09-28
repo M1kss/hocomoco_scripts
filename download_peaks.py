7f7f3a48c1750c0f1bb7fb67e14242107fcae8f8
wish1 <aswq22013@gmail.com>
2021-09-29 02:09:29 +0300

    some fixes

--- download_peaks.py
+++ download_peaks.py
@@ -4,8 +4,11 @@ import pandas as pd
 from tqdm import tqdm
 
 df = pd.read_table('~/mouse/mouse_peaks_dn.tsv')
-df = df[~df['DOWNLOAD_PATH'].isna()]
+df = df[~df['DOWNLOAD_PATH'].isna() & (df['DOWNLOAD_PATH'] != 'None')]
 for index, row in tqdm(df.iterrows()):
+    print(['scp', '-P', '1300', '-T',
+                     'autosome@localhost:' + row['DOWNLOAD_PATH'],
+                      '~/mouse/peaks/'])
     subprocess.Popen(['scp', '-P', '1300', '-T',
                      'autosome@localhost:' + row['DOWNLOAD_PATH'],
                       '~/mouse/peaks/'])
