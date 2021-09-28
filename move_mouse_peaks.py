d8e67539ba9a40c505151647eedef887aaa473e7
wish1 <aswq22013@gmail.com>
2021-09-29 01:05:02 +0300

    some fixes

--- move_mouse_peaks.py
+++ move_mouse_peaks.py
@@ -9,10 +9,14 @@ if len(sys.argv) > 1:
 else:
     raise AssertionError('No meta file provided!')
 master_df = pd.read_table(meta_path, header=None, names=['TF_ID', 'PEAKS'])
-for peak_type in os.listdir(os.path.expanduser('~/grouped_peaks')):
-    os.mkdir(os.path.join(mouse_dir, peak_type))
+for peak_type in os.listdir(os.path.expanduser('~/grouped-peaks')):
+    new_dir = os.path.join(mouse_dir, peak_type)
+    if not os.path.exists(new_dir):
+        os.mkdir(new_dir)
 for index, row in master_df.iterrows():
-    for peak_type in os.listdir(os.path.expanduser('~/grouped_peaks')):
-        base_path = os.path.join(os.path.expanduser('~/grouped_peaks'), peak_type, row['PEAKS'] + '.interval')
+    for peak_type in os.listdir(os.path.expanduser('~/grouped-peaks')):
+        base_path = os.path.join(os.path.expanduser('~/grouped-peaks'), peak_type, row['PEAKS'] + '.interval')
         if os.path.exists(base_path):
             shutil.copy2(base_path, os.path.join(mouse_dir, peak_type, row['PEAKS'] + '.interval'))
+        else:
+            print(base_path)
