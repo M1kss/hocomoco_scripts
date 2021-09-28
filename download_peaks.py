e8c74be5eac9403051e8031a9493b6062ddc8d54
wish1 <aswq22013@gmail.com>
2021-09-29 02:18:36 +0300

    some fixes

--- download_peaks.py
+++ download_peaks.py
@@ -1,3 +1,4 @@
+import os.path
 import time
 import subprocess
 import pandas as pd
@@ -7,7 +8,10 @@ df = pd.read_table('~/mouse/mouse_peaks_dn.tsv')
 df = df[~df['DOWNLOAD_PATH'].isna() & (df['DOWNLOAD_PATH'] != 'None')]
 for index, row in tqdm(df.iterrows(), total=len(df.index)):
     time.sleep(0.01)
-    process = subprocess.Popen(['scp', '-T', '-P', '1300',
-                     'autosome@localhost:' + row['DOWNLOAD_PATH'],
-                      '~/mouse/peaks/'])
-    process.wait()
+    for peak_path in row['DOWNLOAD_PATH'].split():
+        peak_type = os.path.dirname(peak_path)
+        print(peak_type)
+        # process = subprocess.Popen(['scp', '-T', '-P', '1300',
+        #                  'autosome@localhost:' + peak_path,
+        #                   '~/mouse/peaks/'])
+        # process.wait()
