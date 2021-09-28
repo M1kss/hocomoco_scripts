ee675d15410c464cb6aa17038a670b14df8442d9
wish1 <aswq22013@gmail.com>
2021-09-29 02:25:11 +0300

    some fixes

--- download_peaks.py
+++ download_peaks.py
@@ -4,18 +4,16 @@ import subprocess
 import pandas as pd
 from tqdm import tqdm
 
-
 out_path = os.path.expanduser('~/mouse/peaks')
 df = pd.read_table('~/mouse/mouse_peaks_dn.tsv')
 df = df[~df['DOWNLOAD_PATH'].isna() & (df['DOWNLOAD_PATH'] != 'None')]
 for index, row in tqdm(df.iterrows(), total=len(df.index)):
-    time.sleep(0.01)
     for peak_path in row['DOWNLOAD_PATH'].split():
         peak_type = os.path.basename(os.path.dirname(peak_path))
         dir_path = os.path.join(out_path, peak_type)
         if not os.path.exists(dir_path):
             os.mkdir(dir_path)
         process = subprocess.Popen(['scp', '-P', '1300',
-                         'autosome@localhost:' + peak_path,
-                          dir_path])
+                                    'autosome@localhost:' + peak_path,
+                                    dir_path, '>', 'scp0929.log'])
         process.wait()
