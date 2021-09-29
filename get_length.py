bd44e6e231eba378947ea6f02435100eecbc00c7
wish1 <aswq22013@gmail.com>
2021-09-29 14:59:43 +0300

    some fixes

--- get_length.py
+++ get_length.py
@@ -85,7 +85,6 @@ def parse_annotation():
             if row[name] == '__na':
                 row[name] = None
         result[row['id']] = row[['subfamily', 'family']].to_dict()
-    print(result)
     return result
 
 
