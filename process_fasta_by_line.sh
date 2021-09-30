399dd0b7e8e9ac7f2c637cc6b45f3877bab446dc
wish1 <aswq22013@gmail.com>
2021-09-30 17:26:56 +0300

    get length fixed

--- process_fasta_by_line.sh
+++ process_fasta_by_line.sh
@@ -19,10 +19,10 @@ min_len=${ARRAY[7]}
 
 echo $out_path/fasta/${peaks_name}.${peak_type}.${score_type}
 
-#if [ "$motif_type" == 'single' ]; then
-#  java -Xmx4G -cp ~/chipmunk.jar ru.autosome.ChIPHorde ${min_len}:${max_len},${min_len}:${max_len} f c 1.0 m:$out_path/fasta/${peaks_name}.${peak_type}.${score_type}.mfa 100 10 1 2 random auto single 1>$out_path/results/${peaks_name}.${peak_type}.${score_type}.single.out 2>$out_path/logs/${peaks_name}.${peak_type}.${score_type}.single.log
-#fi
-#
-#if [ "$motif_type" == 'flat' ]; then
-#  java -Xmx4G -cp ~/chipmunk.jar ru.autosome.ChIPHorde ${max_len}:${min_len},${max_len}:${min_len} f c 1.0 m:$out_path/fasta/${peaks_name}.${peak_type}.${score_type}.mfa 100 10 1 2 random auto 1>$out_path/results/${peaks_name}.${peak_type}.${score_type}.flat.out 2>$out_path/logs/${peaks_name}.${peak_type}.${score_type}.flat.log
-#fi
+if [ "$motif_type" == 'single' ]; then
+  java -Xmx4G -cp ~/chipmunk.jar ru.autosome.ChIPHorde ${min_len}:${max_len},${min_len}:${max_len} f c 1.0 m:$out_path/fasta/${peaks_name}.${peak_type}.${score_type}.mfa 100 10 1 2 random auto single 1>$out_path/results/${peaks_name}.${peak_type}.${score_type}.single.out 2>$out_path/logs/${peaks_name}.${peak_type}.${score_type}.single.log
+fi
+
+if [ "$motif_type" == 'flat' ]; then
+  java -Xmx4G -cp ~/chipmunk.jar ru.autosome.ChIPHorde ${max_len}:${min_len},${max_len}:${min_len} f c 1.0 m:$out_path/fasta/${peaks_name}.${peak_type}.${score_type}.mfa 100 10 1 2 random auto 1>$out_path/results/${peaks_name}.${peak_type}.${score_type}.flat.out 2>$out_path/logs/${peaks_name}.${peak_type}.${score_type}.flat.log
+fi
