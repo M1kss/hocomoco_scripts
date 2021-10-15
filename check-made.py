e78f19090c9b46947d3d2fd0cd4ce3aa9c30268d
wish1 <aswq22013@gmail.com>
2021-10-15 14:36:37 +0300

    check made

---
+++ check-made.py
@@ -0,0 +1,9 @@
+import sys
+import os
+import pandas as pd
+
+df = pd.read_csv(sys.argv[1], header=None, names=['specie', 'tf', 'exp', 'type', 'ps', 'fs', 'maxl', 'minl'])
+df['path'] = df.apply(lambda x: os.path.join(sys.argv[2], '.'.join([x['exp'], x['type'], x['ps'], x['fs']]) + '.out'), axis=1)
+df = df[df['path'].apply(lambda x: not os.path.isfile(x))]
+print(df)
+df.to_csv(sys.argv[2], index=False, header=False)
