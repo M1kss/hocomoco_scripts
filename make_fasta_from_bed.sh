3887443d1be02d5c6ce7527a34c9bec4f0571306
Sergey Abramov <aswq22013@gmail.com>
2022-01-12 23:13:29 +0300

    results fix

---
+++ make_fasta_from_bed.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+out_path=$1
+genome=$2
+njobs=$4
+
+peaks_name=$(basename $peaks_path)
+for peak_full in $out_path/sorted/*
+do
+  IFS=';'
+  read -ra ADDR <<< "$peak_full"
+  peak_name=ADDR[0]
+  peak_type=ADDR[1]
+  score_type=ADDR[2]
+  if [ $peak_type == "cpics" ] && [ $score_type == 'pvalue' ]
+  then
+    continue
+  fi
+  echo "Now doing $peaks_path/$peak_type/$peak_name, $score_type"
+#  if ! bedtools getfasta -fi $genome -bed $peak_name -name 2>$out_path/logs/${peak_name}.${peak_type}.${score_type}.cut.log | awk -F':' '{print $1}' 1>$out_path/fasta/${peak_name}.${peak_type}.${score_type}.mfa 2>>$out_path/logs/${peak_name}.${peak_type}.${score_type}.cut.log
+#  then
+#    echo "Cut fasta failed: $out_path/sorted/${peak_name}.${peak_type}.${score_type}.sorted.bed"
+#    continue
+#  fi
+done
+
