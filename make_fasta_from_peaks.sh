030c5de43338e6bc6e315f92925f0af5c4164f13
Sergey Abramov <aswq22013@gmail.com>
2022-01-12 22:58:13 +0300

    results fix

--- make_fasta_from_peaks.sh
+++ make_fasta_from_peaks.sh
@@ -26,16 +26,16 @@ do
       continue
     fi
     echo "Now doing $peaks_path/$peak_type/$peak_name, $score_type"
-    if ! python3 format_peaks.py $peaks_path/$peak_type/$peak_name.interval $peak_type $score_type $out_path/raw/${peak_name}.${peak_type}.${score_type}.bed 1>$out_path/logs/${peak_name}.${peak_type}.${score_type}.format.log 2>&1
-    then
-      echo "Format peaks failed: $peaks_path/$peak_type/$peak_name.interval"
-      continue
-    fi
-    if ! bedtools sort -i $out_path/raw/${peak_name}.${peak_type}.${score_type}.bed 1> $out_path/sorted/${peak_name}.${peak_type}.${score_type}.sorted.bed 2>$out_path/logs/${peak_name}.${peak_type}.${score_type}.sort.log
-    then
-      echo "Bed tools failed: $out_path/raw/${peak_name}.${peak_type}.${score_type}.bed"
-      continue
-    fi
+#    if ! python3 format_peaks.py $peaks_path/$peak_type/$peak_name.interval $peak_type $score_type $out_path/raw/${peak_name}.${peak_type}.${score_type}.bed 1>$out_path/logs/${peak_name}.${peak_type}.${score_type}.format.log 2>&1
+#    then
+#      echo "Format peaks failed: $peaks_path/$peak_type/$peak_name.interval"
+#      continue
+#    fi
+#    if ! bedtools sort -i $out_path/raw/${peak_name}.${peak_type}.${score_type}.bed 1> $out_path/sorted/${peak_name}.${peak_type}.${score_type}.sorted.bed 2>$out_path/logs/${peak_name}.${peak_type}.${score_type}.sort.log
+#    then
+#      echo "Bed tools failed: $out_path/raw/${peak_name}.${peak_type}.${score_type}.bed"
+#      continue
+#    fi
     if ! bedtools getfasta -fi $genome -bed $out_path/sorted/${peak_name}.${peak_type}.${score_type}.sorted.bed -name 2>$out_path/logs/${peak_name}.${peak_type}.${score_type}.cut.log | awk -F':' '{print $1}' 1>$out_path/fasta/${peak_name}.${peak_type}.${score_type}.mfa 2>>$out_path/logs/${peak_name}.${peak_type}.${score_type}.cut.log
     then
       echo "Cut fasta failed: $out_path/sorted/${peak_name}.${peak_type}.${score_type}.sorted.bed"
