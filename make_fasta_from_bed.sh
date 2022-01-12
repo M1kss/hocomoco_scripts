743169d26be880edb5b556e8581086c7a145c016
Sergey Abramov <aswq22013@gmail.com>
2022-01-12 23:26:11 +0300

    results fix

--- make_fasta_from_bed.sh
+++ make_fasta_from_bed.sh
@@ -7,7 +7,7 @@ njobs=$3
 for peak_full in $out_path/sorted/*
 do
   IFS='.'
-  read -ra ADDR <<< "$peak_full"
+  read -ra ADDR <<< "$(basename peak_full)"
   peak_name=${ADDR[0]}
   peak_type=${ADDR[1]}
   score_type=${ADDR[2]}
