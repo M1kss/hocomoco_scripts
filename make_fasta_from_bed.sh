dd41f398c8968f33249ad13e6e4c210b17bc9375
Sergey Abramov <aswq22013@gmail.com>
2022-01-12 23:45:40 +0300

    results fix

--- make_fasta_from_bed.sh
+++ make_fasta_from_bed.sh
@@ -11,7 +11,7 @@ do
   peak_name=${ADDR[0]}
   peak_type=${ADDR[1]}
   score_type=${ADDR[2]}
-  echo "Now doing $out_path/fasta/${peak_name}.${peak_type}.${score_type}.mfa"
+  echo "Now doing $out_path/fasta/${peak_name}.${peak_type}.${score_type}.mfa $genome"
   if ! bedtools getfasta "-fi" $genome -bed $peak_name -name 1>"$out_path/fasta/${peak_name}.${peak_type}.${score_type}.mfa" 2>"$out_path/logs/${peak_name}.${peak_type}.${score_type}.cut.log"
   then
     echo "Cut fasta failed: $out_path/sorted/${peak_name}.${peak_type}.${score_type}.sorted.bed"
