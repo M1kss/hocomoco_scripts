bca9e90c1075960baf1faab7393e2bfdded4bb75
Sergey Abramov <aswq22013@gmail.com>
2022-01-13 04:54:45 +0300

    results fix

--- update_length.py
+++ update_length.py
@@ -45,7 +45,9 @@ def main(master_path, out_path):
     convert_d = df.set_index('curated:uniprot_ac')['curated:uniprot_id'].to_dict()
     master = pd.read_csv(master_path, header=None,
                            names=[*common_header, 'Max_len', 'Min_len'])
+    print(len(master.index))
     master = master[master['TF_ID'].apply(lambda x: x in convert_d)]
+    print(len(master.index))
     master['TF_NAME'] = master['TF_ID'].apply(lambda x: convert_d[x] and convert_d[x] == 'CTCF_MOUSE')
     ann_df = pd.read_table(os.path.join('files', 'len_annotated.tsv'))
     len_d = make_length_dict(ann_df)
