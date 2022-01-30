7e6a522caaf307e9480d15ed4f8c16496b8a3c4c
Sergey Abramov <aswq22013@gmail.com>
2022-01-30 17:56:43 +0300

    gem

--- remake_gem.py
+++ remake_gem.py
@@ -25,9 +25,9 @@ for peak in os.listdir(fasta_dir):
                     while df_line[0] not in [x for x in ChromPos.chrs] + ['chrM']:
                         print(line, df_line)
                         df_line = p.readline().strip().split('\t')
-                        if line != '>{}'.format(df_line[3]):
-                            print(line, p.readlines())
-                            raise ValueError
+                    if line != '>{}'.format(df_line[3]):
+                        print(line, p.readlines())
+                        raise ValueError
                     if line != '>{}'.format(df_line[3]):
                         raise AssertionError
                     out.write('> {}\n'.format(int(df_line[3]) - int(df_line[1])))
