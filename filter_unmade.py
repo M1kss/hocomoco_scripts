8fee4dc754e3b56ff333e8fd0714448f25063b6a
Sergey Abramov <aswq22013@gmail.com>
2022-02-15 18:39:41 +0300

    Debuggg

--- filter_unmade.py
+++ filter_unmade.py
@@ -25,7 +25,8 @@ def merge_info_dicts(human_info_dict, mouse_info_dict):
             print(tf)
             raise
         if tf_without_suf in result:
-            result[tf_without_suf] = result[tf_without_suf].append(new_value)
+            old_value = result[tf_without_suf]
+            result[tf_without_suf] = [old_value] + [new_value]
         else:
             result[tf_without_suf] = new_value
         if result[tf_without_suf] is None:
