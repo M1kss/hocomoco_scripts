9ba5da1929406c88f135690ab6221879a363f184
Sergey Abramov <aswq22013@gmail.com>
2022-02-15 23:09:40 +0300

    Debuggg

--- xlsx_report.py
+++ xlsx_report.py
@@ -135,15 +135,15 @@ def process_tf(sheet, t_factor, tf_info, cisbp_dict):
     sheet.write(0, 6, 'HOCOMOCO_sim')
     sheet.write(0, 7, 'HOCOMOCO_info')
     for index, d_type in enumerate(dict_types[1:]):
-        sheet.write(0, index + 7, d_type + '_sim')
-    sheet.write(0, 8 + len(dict_types[1:]), 'Most_sim_motif')
-    sheet.write(0, 9 + len(dict_types[1:]), 'Most_sim_TF')
-    sheet.write(0, 10 + len(dict_types[1:]), 'Most_sim_type')
+        sheet.write(0, 8 + index, d_type + '_sim')
+    sheet.write(0, 9 + len(dict_types[1:]), 'Most_sim_motif')
+    sheet.write(0, 10 + len(dict_types[1:]), 'Most_sim_TF')
+    sheet.write(0, 11 + len(dict_types[1:]), 'Most_sim_type')
     worksheet.freeze_panes(1, 0)  # Freeze the first row. KDIC
     print('Writing to file')
     for index, exp in tqdm(enumerate(sorted_tf_info[:50]), total=len(sorted_tf_info)):
         sheet.set_column(0, 0, name_width)
-        sheet.set_column(4, 5, motif_len * 2.5)
+        sheet.set_column(5, 5, motif_len * 2.5)
         sheet.write(index + 1, 0, exp['name'])
         sheet.write(index + 1, 1, exp['specie'])
         sheet.write(index + 1, 2, exp['selected_by'][:1].capitalize())
