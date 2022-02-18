445c3f0ec9ef3f36c4eb3abea0eb5b66c8c6610c
Sergey Abramov <aswq22013@gmail.com>
2022-02-18 23:44:56 +0300

    fixed correlation

--- xlsx_report.py
+++ xlsx_report.py
@@ -203,6 +203,7 @@ def main():
         sorted_tf_info = [x for x in tf_info if get_max(x)[1] >= 0.01]
         chunk_size = 1000
         parts_start = [i for i in range(0, len(sorted_tf_info), chunk_size)]
+        print('Writing to files')
         with multiprocessing.Pool(len(parts_start)) as pool:
             pool.starmap(write_tf,
                          [('{}.{}.xlsx'.format(tf_name, i//chunk_size + 1),
