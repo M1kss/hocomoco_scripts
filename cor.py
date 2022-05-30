e94840f5326e8fd7b0fd69f4d79fd1eb13ded4f8
Sergey Abramov <aswq22013@gmail.com>
2022-05-31 00:26:16 +0300

    xlsx report

--- cor.py
+++ cor.py
@@ -175,7 +175,7 @@ def main(njobs=10):
     tfs = info_dict.keys()
     tf_dtype = [(tf, d_type) for tf in tfs for d_type
                 in dict_types if tf not in allowed_tfs]
-    with Pool(njobs) as p:
+    with Pool(processes=njobs) as p:
         for tf_dtype, res in zip(tf_dtype, p.starmap(process_tf, [(tf, d_type, dicts, info_dict)
                                                                   for tf, d_type in tf_dtype])):
             if res is None:
