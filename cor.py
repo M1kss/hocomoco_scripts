9eb5f47b068a303b87f35f5a63047182d105ffc7
Sergey Abramov <aswq22013@gmail.com>
2022-05-30 23:41:11 +0300

    allowed tfs excluded

--- cor.py
+++ cor.py
@@ -20,7 +20,7 @@ dict_types = ['hocomoco', 'direct', 'inferred', 'tf_class_subfamily', 'tf_class_
 
 ape_path = os.path.expanduser('~/ape.jar')
 hocomoco_path = 'hocomoco_pwms'
-allowed_tfs = ['YBOX1']
+allowed_tfs = ['YBOX1', 'CTCF', 'ANDR']
 
 species = 'human', 'mouse'
 
@@ -174,7 +174,8 @@ def main(njobs=10):
     info_dict = read_info_dict()
     tfs = info_dict.keys()
     results = {}
-    tf_dtype = [(tf, d_type) for tf in tfs for d_type in dict_types if tf != 'CTCF']
+    tf_dtype = [(tf, d_type) for tf in tfs for d_type
+                in dict_types if tf not in allowed_tfs]
     with Pool(njobs) as p:
         for tf_dtype, res in zip(tf_dtype, p.starmap(process_tf, [(tf, d_type, dicts, info_dict)
                                                                   for tf, d_type in tf_dtype])):
